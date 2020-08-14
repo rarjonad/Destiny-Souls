8ad57b7d8e6230599e0fc34ab283b83c31430f71
arjona <67202305+rarjonad@users.noreply.github.com>
2020-08-14 13:20:19 +0000

    Added Player skills and skill point assignment (#3)
    * Added skill point assign for player skills
    
    * Cleaned up leftover code
    
    * Skill class gets pre-calculated value
    
    * Divided chargen into two smaller functions
    
    * Added menu confirm function, fixed infinite skill point bug

--- scr/menus.py
+++ scr/menus.py
@@ -14,16 +14,35 @@ def calculate_stat_modifiers(stats):
     modifiers = {}
     for i in stats:
         mod = stats[i] // 2 - 5
-        modifiers.update({i + '_mod': mod})
+        modifiers.update({i: mod})
     return modifiers
 
 
-def chargen():
-    name = input("Choose name: ")
-    level = 1
+def calculate_skill_value(skill, modifier):
+    print(skill)
+    skill['value'] = skill['points'] + modifier
+
+
+def menu_confirm(check_parameter):
+    if check_parameter < 0:
+        print('You have assigned more points than the maximum allowed.')
+        input('>...')
+        menu_check = 0
+    elif check_parameter > 0:
+        print('You still have points left to assign')
+        skill_exit = input('Finish? [y/n] ')
+        if skill_exit.lower() == "y":
+            menu_check = 1
+        else:
+            menu_check = 0
+    else:
+        menu_check = 1
+    return menu_check
+
+
+def stat_assign():
     max_points = 72
     points_left = max_points
-    exp = 0
     stat_check = 0
     main_stats = {"STR": 1, "DEX": 1, "CON": 1, "INT": 1, "WIS": 1, "CHA": 1}
     # Loop stat assign
@@ -34,20 +53,11 @@ def chargen():
         print("Minimum value is 1, Maximum value is 18")
         print()
         for i in main_stats:
-            print(i + ": " + str(main_stats[i]) + " | Modifier: " + str(stat_modifiers[i + '_mod']))
+            print(i + ": " + str(main_stats[i]) + " | Modifier: " + str(stat_modifiers[i]))
         input_stat = input("Write stat or 'Done' for exit: ")
         input_stat = input_stat.upper()
         if input_stat == "DONE":
-            if (points_left > 0):
-                print("You still have " + str(points_left) + " points left")
-                point_confirm = input("Finish stat asignment? [y/n] ")
-                if point_confirm.lower() == "y":
-                    stat_check = 1
-            elif (points_left < 0):
-                print("Spent points (" + str(points_left) + ") lower than 0")
-                input(">...")
-            else:
-                stat_check = 1
+            stat_check = menu_confirm(points_left)
         else:
             chosen_stat = main_stats.get(input_stat, 0)
             if chosen_stat == 0:
@@ -64,29 +74,87 @@ def chargen():
                     else:
                         main_stats[input_stat] = stat_increase
                         points_left = max_points - sum(main_stats.values())
+    return main_stats, stat_modifiers
 
-    hp = 10 + stat_modifiers["CON_mod"]
-    mp = 20 + stat_modifiers["INT_mod"]
-    print("hp: " + str(hp))
-    print("mp: " + str(mp))
+
+def skill_assign(stat_modifiers):
+    # SKILL BLOCK
+    print("\nSkills block\n")
     with open('data/player_skills.json') as skills_file:
         skill_list = json.load(skills_file)
+    skill_count = 0
+    for i in skill_list['skills']:  # Add point and value field to every skill for assigned points
+        new_field = {'points': 0, 'value': stat_modifiers[i['stat']]}
+        skill_list['skills'][skill_count].update(new_field)
+        skill_count += 1
+    skill_check = 0
+    skill_points_max = 10 + stat_modifiers["INT"]
+    skill_points_left = skill_points_max
+    while skill_check == 0:  # Skill point assign loop
+        count = 0
+        print('Limit points: ' + str(skill_points_max) + ' Current left: ' + str(skill_points_left))
+        print('\tSkill - Stat - Value - Points - Stat Modifier')
+        print()
+        for i in skill_list['skills']:
+            print(str(count) + ') ' + i['id'] + ' - ' + i['stat'] + ' - ' + str(i['value']) + ' - ' + str(i['points']) + ' - ' + str(stat_modifiers[i['stat']]))
+            count += 1
+        print()
+        chosen_skill = input("Choose Skill by number (empty to finish): ")
+        if chosen_skill == '':
+            skill_check = menu_confirm(skill_points_left)
+        else:
+            try:
+                chosen_skill = int(chosen_skill)
+            except ValueError:
+                pass
+            else:
+                try:
+                    skill_list['skills'][chosen_skill]
+                except IndexError:
+                    print("Skill NOT found")
+                else:
+                    print("Skill found")
+                    skill_found = skill_list['skills'][chosen_skill]
+                    try:
+                        skill_points_assign = int(input("How many points to assign? "))
+                    except ValueError:
+                        pass
+                    else:
+                        if skill_points_assign >= 0:
+                            skill_points_difference = skill_points_assign - skill_found['points']
+                            skill_found['points'] = skill_points_assign
+                            skill_found = calculate_skill_value(skill_found, stat_modifiers[skill_found['stat']])
+                            skill_points_left -= skill_points_difference
+                        else:
+                            print("No negative numbers")
+                            input(">...")
+    return skill_list
+
+
+def chargen():
+    name = input("Choose name: ")
+    # Menu for all chargen options
+    # Generic menu confirmation function
+    main_stats, stat_modifiers = stat_assign()  # Get main stats
+    skill_list = skill_assign(stat_modifiers)  # Get Skills
+    level = 1
+    exp = 0
+    hp = 10 + stat_modifiers["CON"]
+    mp = 20 + stat_modifiers["INT"]
+    print("hp: " + str(hp))
+    print("mp: " + str(mp))
+    # Add inventory
+    # Add Spells
+    # Add Perks/Quirks?
+    # Add Abilities?
     player = pl.Player(name, hp, mp, level, exp, main_stats, stat_modifiers, skill_list)
+    print("\nPlayer data\n")
     print(player)
     print(dir(player))
     print(player.stats)
     print(player.stats_modifiers)
-    print("\nSkills block\n")
-    # Skills
-    skill_check = 0
-    skill_points_max = 10 + stat_modifiers["INT_mod"] * 2
-    points = 2
     for i in player.skills:
         print(player.skills[i])
-
-    # while skill_check == 0:
-    #    for i in skills:
-    #        print(i + ": \t" + str(skills[i]["points"]) + " (" + str(skills[i]["stat"]) + ")")
     return player
 
 
--- scr/player.py
+++ scr/player.py
@@ -9,7 +9,7 @@ class Player():
         self.stats_modifiers = modifiers
         self.skills = {}
         for i in skill_list['skills']:
-            skill_item = {i['id']: Skill(i['id'], i['stat'], 0, self.stats_modifiers[i['stat'] + "_mod"])}
+            skill_item = {i['id']: Skill(i['id'], i['stat'], i['points'], i['value'], self.stats_modifiers[i['stat']])}
             self.skills.update(skill_item)
 
     def __str__(self):
@@ -17,14 +17,15 @@ class Player():
 
 
 class Skill():
-    def __init__(self, name, stat, points, stat_modifier):
+    def __init__(self, name, stat, points, value, stat_modifier):
         self.name = name
         self.stat = stat
         self.points = points
-        self.value = stat_modifier + self.points
+        self.modifier = stat_modifier
+        self.value = value
 
     def __str__(self):
-        return "Skill Object: " + self.name + " - " + self.stat + " - " + str(self.points) + " - " + str(self.value)
+        return "Skill Object: " + self.name + " - " + str(self.value) + " - " + str(self.points) + " - " + self.stat + " - " + str(self.modifier)
 
     def update_value(self, stat_modifier):
         self.value = stat_modifier + self.points
